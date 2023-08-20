3fad851fefaf86ab25bd7a9d5f825ade7f5345ff
Venu Reddy <venu.r@technofacts.co.uk>
2023-08-20 17:28:31 +0530

    changed tag to latest.jdk17

--- build_deploy.sh
+++ build_deploy.sh
@@ -3,7 +3,7 @@
 readonly DOCKER_REGISTRY="docker.io/levernfulcrum"
 readonly DOCKER_IMAGE="lnf-company-service"
 readonly TAG="$(date +%Y%m%d%H%M%S)"
-readonly TAG_LATEST="latest"
+readonly TAG_LATEST="latest.jdk17"
 readonly IMG=$DOCKER_REGISTRY/$DOCKER_IMAGE:$TAG
 readonly IMG_LATEST=$DOCKER_REGISTRY/$DOCKER_IMAGE:$TAG_LATEST
 
