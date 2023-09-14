9f48bd75e939ece952dc088f909781fd4d2f95e4
durgasaigandreddy <durgasai.g@techno-facts.com>
2023-09-14 15:53:54 +0530

    added changes

--- src/main/java/com/technofacts/lnf/company/model/PayrollConfiguration.java
+++ src/main/java/com/technofacts/lnf/company/model/PayrollConfiguration.java
@@ -15,14 +15,14 @@ import lombok.*;
 @Table(name = "payroll_configuration")
 public class PayrollConfiguration extends AuditableEntity{
 
+    @Column(name = "component_code", nullable = false)
+    private String componentCode;
+
     @Column(name = "component_name", nullable = false)
     private String componentName;
 
     @Column(name = "component_value", nullable = false)
     private String  componentValue;
-
-    @Column(name = "component_code", nullable = false)
-    private String componentCode;
 
     @Column(name = "percentage", nullable = false)
     private Double percentage;
