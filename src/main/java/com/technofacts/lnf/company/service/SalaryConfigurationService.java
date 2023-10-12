c54caa16bac594d2ec3f267e78ba0bae5dc37304
vamsi <vamsireddy.v@techno-facts.com>
2023-10-12 12:43:08 +0530

    modified salary component configuration

---
+++ src/main/java/com/technofacts/lnf/company/service/SalaryConfigurationService.java
@@ -0,0 +1,30 @@
+package com.technofacts.lnf.company.service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class SalaryConfigurationService {
+
+    public JsonNode getDesiredSalaryConfigurations() {
+        try {
+            Resource resource = new ClassPathResource("SalaryConfigurations.json");
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonData = objectMapper.readTree(resource.getInputStream());
+            JsonNode salaryComponents = jsonData.get("salary_components");
+            JsonNode salaryDeductions = jsonData.get("salary_deductions");
+            ObjectMapper newObjectMapper = new ObjectMapper();
+            ObjectNode desiredJson = newObjectMapper.createObjectNode();
+            desiredJson.set("salary_components", salaryComponents);
+            desiredJson.set("salary_deductions", salaryDeductions);
+
+            return desiredJson;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to retrieve salary configurations.", e);
+        }
+    }
+}
