347d2e21db744971413242fcf0b04a47c38b39d1
ajith <ajith@technofacts.co.uk>
2024-02-13 07:43:21 +0000

    HRMW-842 - Fix for keycloak configuration

--- src/main/java/com/technofacts/lnf/company/restapi/FileClientImpl.java
+++ src/main/java/com/technofacts/lnf/company/restapi/FileClientImpl.java
@@ -1,10 +1,10 @@
 package com.technofacts.lnf.company.restapi;
 
-import com.technofacts.lnf.exception.LnFEntityNotFoundException;
 import com.technofacts.lnf.exception.LnFException;
 import com.technofacts.lnf.service.File.FileUploadService;
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
@@ -12,12 +12,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.MultipartBodyBuilder;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.util.List;
 
@@ -25,74 +24,82 @@ import java.util.List;
 @Service
 @Transactional
 @Slf4j
-public class FileClientImpl implements FileUploadService {
+public class FileClientImpl extends BaseWebClientService implements FileUploadService {
 
     private final WebClient webClient;
-
-    public FileClientImpl(@Qualifier("fileService") WebClient webClient) {
-        this.webClient = webClient;
-    }
 
     @Value("${aws.s3.bucket.service}")
     private String s3Service;
 
+    @Autowired
+    public FileClientImpl(@Qualifier("fileService") WebClient webClient) {
+        this.webClient = webClient;
+    }
+
     @Override
     public String uploadFile(String folder, MultipartFile file) {
+        // Build the multipart body
         MultipartBodyBuilder bodyBuilder = new MultipartBodyBuilder();
         bodyBuilder.part("folder", folder);
         bodyBuilder.part("file", file.getResource());
-        Jwt jwt = getJwtToken();
 
         try {
-            return webClient.post()
+            // Create the web request, adding JWT token if available
+            WebClient.RequestHeadersSpec<?> spec = webClient.post()
                     .uri(s3Service + "/upload")
-                    .headers(header -> header.setBearerAuth(jwt.getTokenValue()))
                     .contentType(MediaType.MULTIPART_FORM_DATA)
-                    .body(BodyInserters.fromMultipartData(bodyBuilder.build()))
-                    .retrieve()
+                    .body(BodyInserters.fromMultipartData(bodyBuilder.build()));
+
+            // Conditionally add the JWT token to the request headers
+            addJwtToken(spec);
+
+            // Execute the request and block to get the response, consider using subscribe for a non-blocking approach
+            return spec.retrieve()
                     .bodyToMono(String.class)
                     .block();
-        } catch (LnFEntityNotFoundException ex) {
-            log.error("File Upload for company Is Failed{}",ex.getMessage());
+        } catch (WebClientResponseException e) {
+            log.error("File Upload failed. Status code: {}, Message: {}", e.getStatusCode(), e.getMessage(), e);
+            throw new LnFException("File Upload failed" + e);
+        } catch (RuntimeException e) {
+            log.error("Unexpected error occurred during file upload", e);
+            throw new LnFException("File Upload for employee failed" + e);
         }
-        return null;
     }
 
     @Override
     public void delete(List<String> filePaths) {
         try {
             String joinedKeys = String.join(",", filePaths);
-            webClient
-                .delete()
-                .uri(s3Service  +"?filePaths=" + joinedKeys)
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .retrieve()
-                .toBodilessEntity()
-                .block();
+            WebClient.RequestHeadersSpec<?> spec = webClient
+                    .delete()
+                    .uri(s3Service  +"?filePaths=" + joinedKeys)
+                    .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+            addJwtToken(spec);
+            spec.retrieve()
+                    .toBodilessEntity()
+                    .block();
         } catch (Exception e) {
-            log.error("Failed to delete files with filePaths {}: {}", filePaths, e.getMessage());
-            throw new LnFException("Failed to delete files with filePaths " + filePaths, e);
+            log.error("Failed to delete files with keys {}: {}", filePaths, e.getMessage());
+            throw new LnFException("Failed to delete files with keys " + filePaths, e);
         }
     }
 
     public ResponseEntity<byte[]> findFile(String filePath) {
         try {
-            ResponseEntity<byte[]> response = webClient.get()
+            WebClient.RequestHeadersSpec<?> spec = webClient.get()
                     .uri(s3Service + "?filePath={filePath}", filePath)
-                    .accept(MediaType.APPLICATION_OCTET_STREAM)
+                    .accept(MediaType.APPLICATION_OCTET_STREAM);
+            addJwtToken(spec);
+            ResponseEntity<byte[]> response = spec
                     .retrieve()
                     .toEntity(byte[].class)
                     .block();
             log.info("file is retrieved");
             return response;
         } catch (Exception ex) {
-            log.error("File is not retrieved{}", ex.getMessage());
+            log.error("File is not retrieved {}", ex.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
-    }
-
-    private Jwt getJwtToken() {
-        return (Jwt) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 
 }
