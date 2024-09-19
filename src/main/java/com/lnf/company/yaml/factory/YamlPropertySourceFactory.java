361cfb87eeb24b80b3716769bd31f4c74fd6c0b0
durgasaigandreddy <durgasai.g@techno-facts.com>
2024-09-19 09:59:26 +0530

    changed package names and added copyright information

--- src/main/java/com/lnf/company/yaml/factory/YamlPropertySourceFactory.java
+++ src/main/java/com/lnf/company/yaml/factory/YamlPropertySourceFactory.java
@@ -16,19 +16,18 @@
 
 package com.lnf.company.yaml.factory;
 
-import java.io.IOException;
-import java.util.Properties;
-
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.core.env.PropertiesPropertySource;
 import org.springframework.core.env.PropertySource;
 import org.springframework.core.io.support.EncodedResource;
 import org.springframework.core.io.support.PropertySourceFactory;
 
+import java.util.Properties;
+
 public class YamlPropertySourceFactory implements PropertySourceFactory {
 
     @Override
-    public PropertySource<?> createPropertySource(String name, EncodedResource encodedResource) throws IOException {
+    public PropertySource<?> createPropertySource(String name, EncodedResource encodedResource) {
         YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
         factory.setResources(encodedResource.getResource());
 
