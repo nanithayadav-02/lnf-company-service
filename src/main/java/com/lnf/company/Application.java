e9b028c288566598ea18924c860928a7d847be23
suresh <suresh.k@techno-facts.com>
2024-12-03 12:16:19 +0530

    Company--Spring-boot-and-java-upgrade

--- src/main/java/com/lnf/company/Application.java
+++ src/main/java/com/lnf/company/Application.java
@@ -18,8 +18,10 @@ package com.lnf.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
