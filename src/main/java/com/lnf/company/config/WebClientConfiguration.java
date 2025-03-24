40cb8fdfa41c8061602d610d551e269c770c458d
Vamsi Reddy <vamsi.v@levernfulcrum.com>
2025-03-24 17:09:32 +0530

    spring cloud client service

--- src/main/java/com/lnf/company/config/WebClientConfiguration.java
+++ src/main/java/com/lnf/company/config/WebClientConfiguration.java
@@ -19,12 +19,17 @@ package com.lnf.company.config;
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.exporter.otlp.http.trace.OtlpHttpSpanExporter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 
 import java.time.Duration;
@@ -69,9 +74,36 @@ public class WebClientConfiguration {
 
         return WebClient.builder()
                 .baseUrl(baseUrl)
+                .filter(addTracingHeaders())
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .exchangeStrategies(exchangeStrategies)
                 .build();
     }
 
+    @Bean
+    OtlpHttpSpanExporter otlpHttpSpanExporter(@Value("${tracing.url}") String url) {
+        return OtlpHttpSpanExporter.builder()
+                .setEndpoint(url)
+                .build();
+    }
+
+    private ExchangeFilterFunction addTracingHeaders() {
+        return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
+            Span currentSpan = Span.current();
+
+            if (currentSpan.getSpanContext().isValid()) {
+                ClientRequest.Builder requestBuilder = ClientRequest.from(clientRequest)
+                        .header("traceparent", getTraceParentHeader(currentSpan));
+
+                return Mono.just(requestBuilder.build());
+            }
+            return Mono.just(clientRequest);
+        });
+    }
+
+    private String getTraceParentHeader(Span span) {
+        return String.format("00-%s-%s-01",
+                span.getSpanContext().getTraceId(),
+                span.getSpanContext().getSpanId());
+    }
 }
