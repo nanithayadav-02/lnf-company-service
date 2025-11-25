dc43ec98f9b800ff12491c1eb49c7d3387281aa8
gandreddydurgasai <durgasai.g@levernfulcrum.com>
2025-11-25 12:45:12 +0530

    added boolean flag to control events and notes

---
+++ src/main/java/com/lnf/company/utils/CompanyUtil.java
@@ -0,0 +1,45 @@
+/*
+ *
+ *  * Copyright © 2025 Lever And Fulcrum Solutions (hereinafter referred to as "LNF").
+ *  * All rights reserved.
+ *  *
+ *  * This source code is the proprietary property of LNF
+ *  *
+ *  * Unauthorized copying, redistribution, or modification of this code,
+ *  * via any medium, is strictly prohibited unless expressly authorized
+ *  * in writing by LNF.
+ *  *
+ *  * This code is confidential and intended solely for the use of LNF
+ *  * and its authorized personnel.
+ *
+ */
+
+package com.lnf.company.utils;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class CompanyUtil {
+
+    @SuppressWarnings("unchecked")
+    public Map<String, Object> extractJwtClaims() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof Jwt jwt) {
+            return jwt.getClaims();
+        }
+        return Collections.emptyMap();
+    }
+
+    public String getEmail() {
+        Map<String, Object> claims = extractJwtClaims();
+        return (String) claims.get("email");
+    }
+
+}
